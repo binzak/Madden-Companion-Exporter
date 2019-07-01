b757fc279457fee175b956e3c4a1291f4cf44c32
Robin Stepczak <robin.stepczak@landingfactory.fr>
2019-07-01 15:49:35 +0200

    Update app.js with firebase infoZe

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("key/exportdatamd-firebase-adminsdk-af6s4-cf586d2ea1.json");
+const serviceAccount = require("./firebase-info.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
